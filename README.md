9cc7744c4f30ea5510c32bdb6dc6f803fda0b64b
rakeebhasan09 <mdrakeebhasan1@gmail.com>
2026-04-08 18:00:19 +0600

    Solve Failed to connect to MongoDB Error ECONNREFUSED.

--- README.md
+++ README.md
@@ -1,1 +1,19 @@
 This is the readme file for Blueprint server.
+
+Problem
+
+```bash
+Failed to connect to MongoDB Error: querySrv ECONNREFUSED _mongodb._tcp.cluster0.x65kkeb.mongodb.net at QueryReqWrap.onresolve [as oncomplete] (node:internal/dns/promises:293:17) {
+    errno: undefined,
+    code: 'ECONNREFUSED',
+    syscall: 'querySrv',
+    hostname: '_mongodb._tcp.cluster0.x65kkeb.mongodb.net'
+}
+```
+
+Solve:
+
+```bash
+import dns from "dns";
+dns.setServers(["1.1.1.1", "8.8.8.8"]);
+```
--- src/app.ts
+++ src/app.ts
@@ -2,15 +2,18 @@ import cors from "cors";
 import express, { Application, Request, Response } from "express";
 import router from "./routes";
 
+import dns from "dns";
+dns.setServers(["1.1.1.1", "8.8.8.8"]);
+
 const app: Application = express();
 
 // Parsers
 app.use(express.json());
 app.use(
-	cors({
-		origin: "http://localhost:3000",
-		credentials: true,
-	}),
+    cors({
+        origin: "http://localhost:3000",
+        credentials: true,
+    }),
 );
 
 // Application routes
@@ -18,15 +21,15 @@ app.use("/api/v1", router);
 
 // Testing route
 app.get("/", (req: Request, res: Response) => {
-	res.send("Blueprint Server is running!");
+    res.send("Blueprint Server is running!");
 });
 
 // Not found route
 app.use((req: Request, res: Response) => {
-	res.status(404).json({
-		success: false,
-		message: "Route not found",
-	});
+    res.status(404).json({
+        success: false,
+        message: "Route not found",
+    });
 });
 
 export default app;
