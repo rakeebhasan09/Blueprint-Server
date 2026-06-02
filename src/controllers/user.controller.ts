6142871c8bc3f01cd78710fc8acdbae2439e784e
rakeebhasan09 <mdrakeebhasan1@gmail.com>
2026-06-02 11:30:29 +0600

    Fix login route error.

--- src/controllers/user.controller.ts
+++ src/controllers/user.controller.ts
@@ -96,6 +96,7 @@ const createUser = async (req: Request, res: Response) => {
 const loginUser = async (req: Request, res: Response) => {
     try {
         const { email, password } = req.body;
+
         if (!email || !password) {
             return res.status(400).json({
                 success: false,
@@ -119,6 +120,7 @@ const loginUser = async (req: Request, res: Response) => {
                 message: "Invalid password",
             });
         }
+
         res.status(200).json({
             success: true,
             message: "Login successful",
