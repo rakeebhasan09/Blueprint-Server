c35635c2d049e81537f7063e5b7114fcfaaba5d1
rakeebhasan09 <mdrakeebhasan1@gmail.com>
2026-06-28 00:03:14 +0600

    Get all contact's server code added.

--- src/controllers/contact.controller.ts
+++ src/controllers/contact.controller.ts
@@ -24,6 +24,31 @@ const createContact = async (req: Request, res: Response) => {
     }
 };
 
+const getContacts = async (req: Request, res: Response) => {
+    try {
+        const contactMessages = await Contact.find().sort({
+            createdAt: -1,
+        });
+        if (!contactMessages) {
+            return res.status(404).json({
+                success: false,
+                message: "No Message Found",
+            });
+        }
+        res.status(200).json({
+            success: true,
+            contacts: contactMessages,
+        });
+    } catch (error) {
+        console.error(error);
+        res.status(500).json({
+            success: false,
+            message: "Failed to fetch contacts.",
+        });
+    }
+};
+
 export const ContactController = {
     createContact,
+    getContacts,
 };
--- src/routes/contact.route.ts
+++ src/routes/contact.route.ts
@@ -2,6 +2,7 @@ import express from "express";
 import { ContactController } from "../controllers/contact.controller";
 
 const router = express.Router();
+router.get("/", ContactController.getContacts);
 router.post("/", ContactController.createContact);
 
 export const ContactRoutes = router;
